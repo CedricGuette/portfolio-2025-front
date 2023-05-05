00b7673074262156b7fa8c95766abceed245bb56
CedricGuette <cedric.guette@gmail.com>
2023-05-05 20:23:31 +0200

    link again

--- src/components/pages/LaPanthere.tsx
+++ src/components/pages/LaPanthere.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function LaPanthere() {
     return (
-        <div><Link to='../portfolio'>Retour au portfolio</Link><a href='../lapantherebefore/index.html'>AVANT</a> <a href='../lapanthereafter/index.html'>APRES</a> <a href='../lapanthereafter/rapport.pdf'>RAPPORT</a></div>
+        <div><Link to='../portfolio'>Retour au portfolio</Link><a href='../../lapantherebefore/index.html'>AVANT</a> <a href='../../lapanthereafter/index.html'>APRES</a> <a href='../../lapanthereafter/rapport.pdf'>RAPPORT</a></div>
     )
 }
 
